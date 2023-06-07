c827db9776f6d09dd0bd9b532e55633a19e2006a
Estel Collin <estelcollin@MacBook-Pro-de-Estel.local>
2023-06-08 01:50:28 +0200

    8juin

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,24 +1,31 @@
 'use strict';
-//BurgerMenu Design
- const burgerMenu = document.querySelector('.burger__menu');
 
- burgerMenu.addEventListener('click', function() {
-   this.classList.toggle('designopen');
- });
+// BurgerMenu Design
+const burgerMenu = document.querySelector('.burger__menu');
 
-//Button MenuBurger
-let TriggerButton = document.querySelector(".burger__menu");
-let menu = document.querySelector(".nav__menu");
-
-TriggerButton.addEventListener("click", triggerListener);
-
-function triggerListener(){
-    menu.classList.toggle('menu--open');
+if (burgerMenu) {
+  burgerMenu.addEventListener('click', function () {
+    this.classList.toggle('designopen');
+  });
 }
 
 
 
-//NAV ACTIVE
+// Button MenuBurger
+let TriggerButton = document.querySelector(".burger__menu");
+let menu = document.querySelector(".nav__menu");
+
+if (TriggerButton && menu) {
+  TriggerButton.addEventListener("click", triggerListener);
+
+  function triggerListener() {
+    menu.classList.toggle('menu--open');
+  }
+}
+
+
+
+// NAV ACTIVE
 let navTriggers = document.querySelectorAll(".menu__el");
 for (let navTrigger of navTriggers) {
   navTrigger.addEventListener("click", (event) => {
@@ -32,126 +39,103 @@ for (let navTrigger of navTriggers) {
 
 
 
-//NAV ACTIVE SCROLL
-if (TriggerButton != undefined && TriggerButton != null){
+// NAV ACTIVE SCROLL
+if (TriggerButton && document.querySelectorAll('.section').length > 0) {
   let section = document.querySelectorAll('.section');
-  const navTriggers = document.querySelectorAll(".menu__el");
-  function activeLink(li){
+
+  function activeLink(li) {
     navTriggers.forEach((item) => item.classList.remove('menu__el--active'));
     li.classList.add('menu__el--active');
-  };
+  }
+
   navTriggers.forEach((item) =>
-  item.addEventListener('click', function(){
-    activeLink(this);
-  }));
+    item.addEventListener('click', function () {
+      activeLink(this);
+    })
+  );
+
   window.onscroll = () => {
     section.forEach(sec => {
       let top = window.scrollY;
       let offset = sec.offsetTop;
       let height = sec.offsetHeight;
       let id = sec.getAttribute('id');
-      if (top >= offset && top < offset + height){
-        const target = document.querySelector(`[href='#${id}']`).parentElement;
-        activeLink(target);
-      };
+      if (top >= offset && top < offset + height) {
+        const target = document.querySelector(`[href='#${id}']`);
+        if (target && target.parentElement) {
+          activeLink(target.parentElement);
+        }
+      }
     });
   };
-};
+}
 
 
 
-//PARALLAX HEADER
+// PARALLAX HEADER
 var layers = document.querySelectorAll('.div__parallax');
 
-function parallax(){
-    
-  if (window.matchMedia("(min-width: 600px)").matches){
-
+function parallax() {
+  if (window.matchMedia("(min-width: 600px)").matches) {
     var scrollTop = window.pageYOffset;
-
-    layers.forEach(function(layer){
-        var speed = layer.getAttribute('data-speed');
-        var yPos = scrollTop * speed / 100;
-        layer.style.transform = 'translate3d(0px, ' + yPos + 'px, 0px)';
+    layers.forEach(function (layer) {
+      var speed = layer.getAttribute('data-speed');
+      var yPos = scrollTop * speed / 100;
+      layer.style.transform = 'translate3d(0px, ' + yPos + 'px, 0px)';
     });
-
-  }else{
-
-  }   
-
+  }
 }
 window.addEventListener('scroll', parallax);
 
 
 
-
-//SCROLL TIKTOK
+// SCROLL TIKTOK
 var upItem = document.querySelector('.editor__list--up');
 var centerItem = document.querySelector('.editor__list--center');
 
-window.addEventListener("scroll", function () {
-
-  if (window.matchMedia("(min-width: 600px)").matches) {
-
-    upItem.style.transform = "translateX(" + -window.pageYOffset / 40 + "px)";
-    centerItem.style.transform = "translateX(" + window.pageYOffset / 20 + "px)";
-  
-  } else {
-   
-  }
-
-});
+if (upItem && centerItem) {
+  window.addEventListener("scroll", function () {
+    if (window.matchMedia("(min-width: 600px)").matches) {
+      upItem.style.transform = "translateX(" + -window.pageYOffset / 40 + "px)";
+      centerItem.style.transform = "translateX(" + window.pageYOffset / 20 + "px)";
+    }
+  });
+}
 
 
 
-//Bouton rencontre utlisateur
+// Bouton rencontre utilisateur
 var showButtons = document.querySelectorAll('.showButton');
 var hideButtons = document.querySelectorAll('.hideButton');
 var actionButtons = document.querySelectorAll('.action__button');
 
-showButtons.forEach(function(button, index) {
-  button.addEventListener('click', function() {
+showButtons.forEach(function (button, index) {
+  button.addEventListener('click', function () {
     actionButtons[index].style.display = 'block';
   });
 });
 
-hideButtons.forEach(function(button, index) {
-  button.addEventListener('click', function() {
+hideButtons.forEach(function (button, index) {
+  button.addEventListener('click', function () {
     actionButtons[index].style.display = 'none';
   });
 });
 
+hideButtons.forEach
 
 
-//graphique batonnet
+
+// Graphique bâtonnet
 const bars = document.querySelectorAll('.bargraph');
 const barDescription = document.getElementById('bar-description');
 
 bars.forEach(bar => {
-  bar.addEventListener('mouseover', function() {
+  bar.addEventListener('mouseover', function () {
     const label = this.getAttribute('data-label');
     barDescription.textContent = label;
   });
 
-  bar.addEventListener('mouseout', function() {
+  bar.addEventListener('mouseout', function () {
     barDescription.textContent = '';
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
